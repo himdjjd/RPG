4ed629ecd98b08dde6b7fcdce77ec7c36d8bd7e9
inScope <knnth@inscopestudios.com>
2019-03-24 06:10:00 +0100

    26.1
    Added a recipe script

---
+++ Assets/Scripts/Profession/Profession.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Profession : MonoBehaviour
+{
+    private Recipe selectedRecipe;
+
+    public void ShowDescription(Recipe recipe)
+    {
+        if (selectedRecipe != null)
+        {
+            selectedRecipe.Deselect();
+        }
+
+        this.selectedRecipe = recipe;
+
+        this.selectedRecipe.Select();
+    }
+}
---
+++ Assets/Scripts/Profession/Recipe.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Recipe : MonoBehaviour
+{
+
+    [SerializeField]
+    private CraftingMaterial[] materials;
+
+    [SerializeField]
+    private Item output;
+
+    [SerializeField]
+    private int outputCount;
+
+    [SerializeField]
+    private string description;
+
+    [SerializeField]
+    private Image highlight;
+
+    public Item Output
+    {
+        get
+        {
+            return output;
+        }
+    }
+
+    public int OutputCount
+    {
+        get
+        {
+            return outputCount;
+        }
+
+        set
+        {
+            outputCount = value;
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return description;
+        }
+    }
+
+    public CraftingMaterial[] Materials
+    {
+        get
+        {
+            return materials;
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = output.MyTitle;
+    }
+
+    public void Select()
+    {
+        Color c = highlight.color;
+        c.a = .3f;
+        highlight.color = c;
+    }
+
+    public void Deselect()
+    {
+        Color c = highlight.color;
+        c.a = 0f;
+        highlight.color = c;
+
+    }
+
+}
