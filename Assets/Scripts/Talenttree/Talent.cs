96b0ae313c49ec6bd2a7244b2ec80e359ea28901
inScope <knnth@inscopestudios.com>
2019-10-12 23:05:02 +0200

    31.1

---
+++ Assets/Scripts/Talenttree/Talent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talent : MonoBehaviour
+{
+    private Image sprite;
+
+    [SerializeField]
+    private Text countText;
+
+    private void Awake()
+    {
+        sprite = GetComponent<Image>();
+
+    }
+
+    public void Lock()
+    {
+        sprite.color = Color.gray;
+        countText.color = Color.gray;
+    }
+
+    public void Unlock()
+    {
+        sprite.color = Color.white;
+        countText.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/Talenttree/TalentTree.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalentTree : MonoBehaviour
+{
+
+    [SerializeField]
+    private Talent[] talents;
+
+    [SerializeField]
+    private Talent[] unlockedByDefault;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTalents();
+    }
+
+
+    private void ResetTalents()
+    {
+        foreach (Talent talent in talents)
+        {
+            talent.Lock();
+        }
+
+        foreach (Talent talent in unlockedByDefault)
+        {
+            talent.Unlock();
+        }
+    }
+}
