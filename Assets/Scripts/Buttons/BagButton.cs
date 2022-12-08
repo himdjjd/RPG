bf464d823922e60847c77f09a4046ea299d69aac
himdjjd <86347965+himdjjd@users.noreply.github.com>
2022-12-08 14:19:10 +0800

    ASSETS

---
+++ Assets/Scripts/Buttons/BagButton.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BagButton : MonoBehaviour, IPointerClickHandler
+{
+    private static BagButton instance;
+
+    public static BagButton MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<BagButton>();
+            }
+
+            return instance;
+        }
+    }
+    /// <summary>
+    /// A reference to the bag item
+    /// </summary>
+    private Bag bag;
+
+    [SerializeField]
+    private GameObject closeBtn;
+    /// <summary>
+    /// Sprites to indicate if the bag is full or empty
+    /// </summary>
+    //[SerializeField]
+    //private Sprite full, empty;
+
+    //[SerializeField]
+    //private int bagIndex;
+
+    /// <summary>
+    /// A property for accessing the specific bag
+    /// </summary>
+    public Bag MyBag
+    {
+        get
+        {
+            return bag;
+        }
+
+        set
+        {
+            //if (value != null)
+            //{
+            //    GetComponent<Image>().sprite = full;
+            //}
+            //else
+            //{
+            //    GetComponent<Image>().sprite = empty;
+            //}
+
+            bag = value;
+        }
+    }
+
+    //public int MyBagIndex
+    //{
+    //    get
+    //    {
+    //        return bagIndex;
+    //    }
+
+    //    set
+    //    {
+    //        bagIndex = value;
+    //    }
+    //}
+
+    /// <summary>
+    /// if we click the specific bag button
+    /// </summary>
+    /// <param name="eventData"></param>
+    [System.Obsolete]
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            //if (InventoryScript.MyInstance.FromSlot != null && HandScript.MyInstance.MyMoveable != null && HandScript.MyInstance.MyMoveable is Bag)
+            //{
+            //    if (MyBag != null)
+            //    {
+            //        InventoryScript.MyInstance.SwapBags(MyBag, HandScript.MyInstance.MyMoveable as Bag);
+            //    }
+            //    else
+            //    {
+            //        Bag tmp = (Bag)HandScript.MyInstance.MyMoveable;
+            //        tmp.MyBagButton = this;
+            //        tmp.Use();
+            //        MyBag = tmp;
+            //        HandScript.MyInstance.Drop();
+            //        InventoryScript.MyInstance.FromSlot = null;
+            //    }
+
+            //else if (Input.GetKey(KeyCode.LeftShift))
+            //{
+            //    HandScript.MyInstance.TakeMoveable(MyBag);
+            //}
+            //else 
+            if (bag != null)//If we have a bag equipped
+            {
+                //Open or close the bag
+                bag.MyBagScript.OpenClose();
+                if (closeBtn.active==true)
+                {
+                    closeBtn.SetActive(false);
+                }
+                else
+                {
+                    closeBtn.SetActive(true);
+                }
+            }
+        }
+        }
+
+  
+    
+
+/// <summary>
+/// Removes the bag from the bagbar
+/// </summary>
+//public void RemoveBag()
+//{
+//    InventoryScript.MyInstance.RemoveBag(MyBag);
+//    MyBag.MyBagButton = null;
+
+//    foreach (Item item in MyBag.MyBagScript.GetItems())
+//    {
+//        InventoryScript.MyInstance.AddItem(item);
+//    }
+
+//    MyBag = null;
+//}
+
+    public void Close()
+    {
+        closeBtn.SetActive(false);
+        bag.MyBagScript.OpenClose();
+    }
+}
---
+++ Assets/Scripts/Other/HandScript.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HandScript : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton instance of the handscript
+    /// </summary>
+    private static HandScript instance;
+
+    public static HandScript MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<HandScript>();
+            }
+
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// The current moveable
+    /// </summary>
+    public IMoveable MyMoveable { get; set; }
+
+    /// <summary>
+    /// The icon of the item, that we acre moving around atm.
+    /// </summary>
+    private Image icon;
+
+    /// <summary>
+    /// An offset to move the icon away from the mouse
+    /// </summary>
+    [SerializeField]
+    private Vector3 offset;
+
+    [SerializeField]
+    private GameObject dialogue;
+
+    [SerializeField]
+    private Text dialogueText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Creates a reference to the image on the hand
+        icon = GetComponent<Image>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Makes sure that the icon follows the hand
+        icon.transform.position = Input.mousePosition+offset;
+
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject() && MyInstance.MyMoveable != null)
+        {
+            DeleteItem();
+        }
+
+      
+	}
+
+    /// <summary>
+    /// Take a moveable in the hand, so that we can move it around
+    /// </summary>
+    /// <param name="moveable">The moveable to pick up</param>
+    string moveableText = null;
+    public void TakeMoveable(IMoveable moveable,Item item)
+    {
+        this.MyMoveable = moveable;
+        icon.sprite = moveable.MyIcon;
+        icon.enabled = true;
+        moveableText = item.MyTitle;
+    }
+
+    public void SpellTakeMoveable(IMoveable moveable)
+    {
+        this.MyMoveable = moveable;
+        icon.sprite = moveable.MyIcon;
+        icon.enabled = true;
+    }
+
+    public IMoveable Put()
+    {
+        IMoveable tmp = MyMoveable;
+        MyMoveable = null;
+        icon.enabled = false;
+        return tmp;
+    }
+
+    public void Drop()
+    {
+        MyMoveable = null;
+        icon.enabled = false;
+        InventoryScript.MyInstance.FromSlot = null;
+        CloseDialogue();
+    }
+
+    public void DropTrue()
+    {
+        if (MyMoveable is Item)
+        {
+            Item item = (Item)MyMoveable;
+            if (item.MySlot != null)
+            {
+                item.MySlot.Clear();
+            }
+            else if (item.MyCharButton != null)
+            {
+                item.MyCharButton.DequipArmor();
+            }
+
+        }
+
+        InventoryScript.MyInstance.FromSlot = null;
+
+        MyMoveable = null;
+        icon.enabled = false;
+        InventoryScript.MyInstance.FromSlot = null;
+        CloseDialogue();
+    }
+
+    public void DropFalse()
+    {
+        CloseDialogue();
+      
+    }
+
+    /// <summary>
+    /// Deletes an item from the inventory
+    /// </summary>
+    public void DeleteItem()
+    {
+
+
+        ShowDialogue(moveableText);
+
+
+    }
+
+    public void ShowDialogue(string text)
+    {
+        if (text == null)
+        {
+            dialogueText.text = "取消操作？";
+        }
+        else
+        {
+            dialogueText.text = "你要丢弃" + text + "吗？";
+        }
+
+       
+
+        dialogue.SetActive(true);
+    }
+
+    public void CloseDialogue()
+    {
+        moveableText = null;
+        dialogue.SetActive(false);
+    }
+}
